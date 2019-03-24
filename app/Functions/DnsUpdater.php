aa891a74814adf6d6f89144d397f0520f064bc63
rumd3x <edmurcardoso@gmail.com>
2019-03-24 15:39:49 -0300

    added logging
    Signed-off-by: rumd3x <edmurcardoso@gmail.com>

--- app/Functions/DnsUpdater.php
+++ app/Functions/DnsUpdater.php
@@ -4,6 +4,8 @@ namespace DnsUpdater\Functions;
 use DnsUpdater\Classes\IPv4Address;
 use DnsUpdater\Providers\DnsProviderInterface;
 use DnsUpdater\Exceptions\InvalidProviderException;
+use Monolog\Logger;
+use Monolog\Handler\ErrorLogHandler;
 
 final class DnsUpdater
 {
@@ -20,15 +22,35 @@ final class DnsUpdater
      */
     public function updateDnsRecord()
     {
+        $logger = new Logger("DNS-Updater");
+        $logger->pushHandler(new ErrorLogHandler());
+
+        $logger->info("Getting current IP...");
         $currentIp = $this->getCurrentIp();
+        $logger->info(sprintf("Current IP: %s", $currentIp));
+
+        $logger->info(sprintf("Trying connect to \"%s\"...", getenv('PROVIDER')));
         $dnsProvider = $this->getProviderInstance();
+        $logger->info(sprintf("Connected successfully using implementation: ", get_class($dnsProvider)));
+
+        $logger->info("Getting DNS Record from provider...");
         $record = $dnsProvider->getRecord();
+        $logger->info(sprintf("Current IP on Provider: %s", $record->getAddress()->getString()));
 
         if ($record->getAddress()->equals($currentIp)) {
+            $logger->info("No change needed. Terminating.");
             return;
         }
 
-        $dnsProvider->updateRecord($currentIp);
+        $logger->info(sprintf("Trying to update Record on Provider: %s -> %s", $record->getAddress()->getString(), $currentIp));
+        $success = $dnsProvider->updateRecord($currentIp);
+
+        if (!$success) {
+            $logger->error("Failed to update DNS Record on provider!");
+            return;
+        }
+
+        $logger->info("DNS Record updated successfully!");
     }
 
     /**
